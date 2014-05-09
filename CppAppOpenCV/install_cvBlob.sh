2fa4033f49f4d1a27d544c5d31e8421369daaeb0
Gary Stafford <gstafford@paychex.com>
2014-05-09 16:02:10 -0400

    Trying to get Travis-CI working with OpenCV and cvBlob

---
+++ CppAppOpenCV/install_cvBlob.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "--- Removing any pre-installed ffmpeg and x264"
+wget -O cvblob-0.10.4-src.tgz https://cvblob.googlecode.com/files/cvblob-0.10.4-src.tgz
+tar xzvf cvblob-0.10.4-src.tgz
+cd cvblob/
+mkdir build
+cd build
+cmake ..
+make
