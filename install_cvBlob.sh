0e56e244c6d8a67dd54ce81717c116cd70c03758
Gary A. Stafford <garystafford@rochester.rr.com>
2014-05-09 22:36:57 -0400

    Updating cvBlob install script.

--- install_cvBlob.sh
+++ install_cvBlob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "--- Removing any pre-installed ffmpeg and x264"
+echo "--- Installing cvBlob..."
 wget -O cvblob-0.10.4-src.tgz https://cvblob.googlecode.com/files/cvblob-0.10.4-src.tgz
 tar xzvf cvblob-0.10.4-src.tgz
 cd cvblob/
@@ -8,3 +8,4 @@ mkdir build
 cd build
 cmake ..
 sudo make install
+echo "--- Installation of cvBlob complete."
